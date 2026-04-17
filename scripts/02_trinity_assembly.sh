a96382c21fc59d79d1d5e9c4e659ba036556c79b
Abigail Meyer <90940282+anonymouslichen@users.noreply.github.com>
2026-04-16 22:27:37 -0500

    Update 02_trinity_assembly.sh
    added filepath and configuration variables to enhance reproducibility

--- scripts/02_trinity_assembly.sh
+++ scripts/02_trinity_assembly.sh
@@ -7,10 +7,12 @@
 #SBATCH --mail-user=YOUR_EMAIL@institution.edu
 
 # ============== USER CONFIGURATION ==============
-PROJECT_DIR="${PROJECT_DIR:-~/my_project}"
-FILTERED_READS_DIR="${PROJECT_DIR}/filtered_reads"
-SAMPLES_FILE="${FILTERED_READS_DIR}/samples.txt"
+PROJECT_DIR="${PROJECT_DIR:-/path/to/project}"
+FILTERED_READS_DIR="${FILTERED_READS_DIR:-${PROJECT_DIR}/filtered_reads}"
+SAMPLES_FILE="${SAMPLES_FILE:-${FILTERED_READS_DIR}/samples.txt}"
 SCRATCH_DIR="${SCRATCH_DIR:-/path/to/scratch/dir}"
+CPU="${SLURM_NTASKS:-16}"
+MAX_MEMORY="${MAX_MEMORY:-238G}"
 # ================================================
 
 cd "${FILTERED_READS_DIR}"
@@ -18,8 +20,8 @@ cd "${FILTERED_READS_DIR}"
 module load trinityrnaseq/2.14.0
 
 Trinity --seqType fq \
-        --max_memory 238G \
+        --max_memory "${MAX_MEMORY}" \
         --samples_file "${SAMPLES_FILE}" \
         --output "${SCRATCH_DIR}" \
-        --CPU 16 \
+        --CPU "${CPU}" \
         --no_distributed_trinity_exec
